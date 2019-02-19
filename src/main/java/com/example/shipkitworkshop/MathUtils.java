c6dcd936440f178d1c916666b027c5ea0c1cedf4
Marcin Stachniuk <marcin.stachniuk@collibra.com>
2019-02-19 09:21:56 +0100

    Reset to the start, remove code

--- src/main/java/com/example/shipkitworkshop/MathUtils.java
+++ src/main/java/com/example/shipkitworkshop/MathUtils.java
@@ -1,31 +1,8 @@
 package com.example.shipkitworkshop;
 
-/**
- * This class is for math operations.
- */
 public class MathUtils {
 
-    /**
-     * This method check is it possible to build triangle from arguments.
-     * @param a a width of first side
-     * @param b a width of second side
-     * @param c a width if 3th side
-     * @return true if you can build triangle, false otherwise.
-     */
     public static boolean isTriangle(int a, int b, int c) {
-        return a + b > c
-                && a + c > b
-                && b + c > a;
-    }
-
-    /**
-     * This method check is it possible to build triangle from arguments.
-     * @param a a width of first side
-     * @param b a width of second side
-     * @param c a width if 3th side
-     * @return true if you can build triangle, false otherwise.
-     */
-    public static boolean isTriangle(float a, float b, float c) {
         return a + b > c
                 && a + c > b
                 && b + c > a;
